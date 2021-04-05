a306b31ee3486c3e83417168e315293d0668ee26
AlvaroCopa <copa730@gmail.com>
2021-04-04 20:12:53 -0400

    Subiendo el ejercicio 3

---
+++ head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es-BO">
+  <head>
+		<title>UMSA</title>
+	</head>
+	<link rel='stylesheet' type='text/css' href='/ejercicio3/style.css' />
+  <body>
+  	<header id="cabeza">
+    	<div>
+      	<div>
+        	<ul> 
+						<h2 class="title">UMSA FCPN</h2>
+						<li><a href="/ejercicio3/Biologia">Biologia</a></li>                
+          	<li><a href="/ejercicio3/Fisica">Fisica</a></li>
+          	<li><a href="/ejercicio3/Informatica">Informatica</a></li>
+          	<li><a href="/ejercicio3/Matematica">Matematica</a></li>
+          	<li><a href="/ejercicio3/Quimica">Quimica</a></li>
+						<li><a href="/ejercicio3">Inicio</a></li>
+        	</ul>
+      	</div>
+    	</div>
+		</header>
